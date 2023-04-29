6111eb5ee637277a268baf7f39387365c88bf8b6
ulf <mail@ulf-westermann.de>
2023-04-29 13:31:33 +0200

    update

--- wiki.py
+++ wiki.py
@@ -19,7 +19,7 @@ class SourceData(pydantic.BaseModel):
 
 
 
-MARKUP_PATH = pathlib.Path("./markup")
+SOURCE_PATH = pathlib.Path("./source")
 WWW_PATH = pathlib.Path("./www")
 WWW_MEDIA_PATH = WWW_PATH / "media"
 
@@ -27,35 +27,27 @@ WWW_MEDIA_PATH = WWW_PATH / "media"
 app = fastapi.FastAPI()
 
 
-@app.get("/api/markup")
-async def get_markup_files():
-    """get list of markup file names"""
-    sources = []
-
-    sources.extend(MARKUP_PATH.glob("*.md"))
-    sources.extend(MARKUP_PATH.glob("*.rst"))
-    sources.extend(MARKUP_PATH.glob("*.txt"))
-    sources.extend(MARKUP_PATH.glob("*.html"))
-    sources.extend(MARKUP_PATH.glob("*.css"))
-
-    return [source.name for source in sources]
+@app.get("/api/source")
+async def get_source_files():
+    """get list of source file names"""
+    return [file.name for file in SOURCE_PATH.glob("*")]
 
 
-@app.get("/api/markup/{name}")
-async def get_markup(name: str):
-    """get markup source for a html page"""
-    markup_file_path = MARKUP_PATH / name
+@app.get("/api/source/{name}")
+async def get_source(name: str):
+    """get source source for a html page"""
+    source_file_path = SOURCE_PATH / name
 
-    if not markup_file_path.is_relative_to(MARKUP_PATH):
+    if not source_file_path.is_relative_to(SOURCE_PATH):
         raise fastapi.HTTPException(status_code=403, detail="not allowed")
 
-    with open(markup_file_path, "r", encoding="utf-8") as file:
+    with open(source_file_path, "r", encoding="utf-8") as file:
         return file.read()
 
 
-@app.delete("/api/markup/{name}")
-async def delete_markup_file(name: str):
-    """delete markup file and corresponding html page"""
+@app.delete("/api/source/{name}")
+async def delete_source_file(name: str):
+    """delete source file and corresponding html page"""
     # check if html file is valid
     html_file_path = WWW_PATH.joinpath(name).with_suffix(".html")
 
@@ -65,60 +57,58 @@ async def delete_markup_file(name: str):
     if html_file_path.name == "manage.html": # manage.html is special page
         raise fastapi.HTTPException(status_code=403, detail="not allowed")
 
-    # check if markup file is valid
-    markup_file_path = MARKUP_PATH.joinpath(name)
+    # check if source file is valid
+    source_file_path = SOURCE_PATH.joinpath(name)
 
-    if not markup_file_path.is_relative_to(MARKUP_PATH):
+    if not source_file_path.is_relative_to(SOURCE_PATH):
         raise fastapi.HTTPException(status_code=403, detail="not allowed")
 
     # delet files
     html_file_path.unlink(missing_ok=True)
-    markup_file_path.unlink()
+    source_file_path.unlink()
 
 
-#@app.put("/api/markup/{name}", response_class=fastapi.responses.HTMLResponse)
-@app.put("/api/markup/{name}")
-async def put_markup_file(name: str, source: SourceData) -> None:
-    """put markup file and create corresponding html page"""
-    # check if markup file path is valid
-    markup_file_path = MARKUP_PATH / name
+#@app.put("/api/source/{name}", response_class=fastapi.responses.HTMLResponse)
+@app.put("/api/source/{name}")
+async def put_source_file(name: str, source: SourceData) -> None:
+    """put source file and create corresponding html page"""
+    # check if source file path is valid
+    source_file_path = SOURCE_PATH / name
 
-    if not markup_file_path.is_relative_to(MARKUP_PATH):
+    if not source_file_path.is_relative_to(SOURCE_PATH):
         raise fastapi.HTTPException(status_code=403, detail="not allowed")
 
-    if markup_file_path.name == "manage.md": # manage.html is a special page
+    if source_file_path.name == "manage.md": # manage.html is a special page
         raise fastapi.HTTPException(status_code=403, detail="not allowed")
 
-    if markup_file_path.suffix not in (".md", ".rst", ".txt", ".html", ".css"):
-        raise fastapi.HTTPException(status_code=400, detail="file type not supported")
-
-    # backup old markup file with same name, if present
+    # backup old source file with same name, if present
     try:
-        markup_file_path.rename(markup_file_path.parent / pathlib.Path(markup_file_path.name + "_" + datetime.datetime.utcnow().isoformat() + ".backup"))
+        source_file_path.rename(source_file_path.parent / pathlib.Path(source_file_path.name + "_" + datetime.datetime.utcnow().isoformat() + ".backup"))
     except FileNotFoundError:
         pass
 
-    # sanitize input and write markup file
-    with open(markup_file_path, "w", encoding="utf-8") as file:
+    # sanitize input and write source file
+    with open(source_file_path, "w", encoding="utf-8") as file:
         # todo: sanitizer removes whitespaces
         #sanitized_html = html_sanitizer.Sanitizer({"keep_typographic_whitespace": True}).sanitize(source.data)
         #sanitized_html = source.data
         file.write(source.data)
 
-    # create html file
-    if markup_file_path.suffix != ".css":
-        css_files = [css_file.name for css_file in MARKUP_PATH.glob("*.css")]
+    # process new source file
+    _process_source_file(source_file_path)
 
-        if not _create_html_page(name, markup_file_path, css_files):
-            raise fastapi.HTTPException(status_code=400, detail="error in source")
+    #if source_file_path.suffix != ".css":
+    #    css_files = [css_file.name for css_file in SOURCE_PATH.glob("*.css")]
+
+    #    if not _create_html_page(name, source_file_path, css_files):
+    #        raise fastapi.HTTPException(status_code=400, detail="error in source")
+
 
 
 @app.get("/api/media")
 async def get_media_files():
     """get list of media files"""
-    files = WWW_MEDIA_PATH.glob("*")
-
-    return [file.name for file in files]
+    return [file.name for file in WWW_MEDIA_PATH.glob("*")]
 
 
 @app.put("/api/media")
@@ -155,31 +145,54 @@ async def delete_media_file(name: str):
     file_path.unlink()
 
 
-def _create_html_page(name: str, path: pathlib.Path, css_files: list) -> bool:
+def _process_source_file(source_file_path: pathlib.Path) -> bool:
+    """create html files from markup sources, then copy files to www directory"""
+
+    # check if we need go generate html by invoking pandoc
+    if source_file_path.suffix  in (".md", ".rst"):
+        # get all currently availabe css files
+        css_files = [css_file.name for css_file in SOURCE_PATH.glob("*.css")]
+
+        
+        return _create_html_page(source_file_path, css_files)
+    else:
+        # copy other files to www directory
+        www_file_path = WWW_PATH / source_file_path.name
+        www_file_path.write_bytes(source_file_path.read_bytes())
+        return True
+
+
+def _create_html_page(source_file_path: pathlib.Path, css_files: list[str]) -> bool:
     run_params = ["pandoc", "--standalone", "--to", "html5"]
 
     for css_file in css_files:
         run_params.extend(["--css", css_file])
 
-    run_params.append(path)
+    run_params.append(source_file_path)
+
+    print(run_params)
 
     result = subprocess.run(run_params, capture_output=True, check=True, shell=False)
 
     if result.returncode != 0:
         return False
 
-    html_file_path = result.stdout.decode("utf-8")
+    html_file_data = result.stdout.decode("utf-8")
 
-    html_path = WWW_PATH.joinpath(name).with_suffix(".html")
+    print(html_file_data)
+
+    html_path = WWW_PATH.joinpath(source_file_path.stem).with_suffix(".html")
+
+    print(html_path)
 
     with open(html_path, "w", encoding="utf-8") as file:
-        file.write(html_file_path)
+        file.write(html_file_data)
 
     return True
 
 
 # create directories if not present
-MARKUP_PATH.mkdir(exist_ok=True)
+SOURCE_PATH.mkdir(exist_ok=True)
 WWW_PATH.mkdir(exist_ok=True)
 WWW_MEDIA_PATH.mkdir(exist_ok=True)
 

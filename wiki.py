cface3e22570a303cfbab1f12ae59a70b58d4fa4
ulf <mail@ulf-westermann.de>
2023-04-30 17:47:39 +0200

    update

--- wiki.py
+++ wiki.py
@@ -5,22 +5,25 @@ import datetime
 import pathlib
 import subprocess
 
-
 import fastapi
 import fastapi.responses
 import fastapi.staticfiles
 import pydantic
 import uvicorn
-#import html_sanitizer
+
+import plugin
 
 
 class SourceData(pydantic.BaseModel):
     data : str
 
 
-SOURCE_PATH = pathlib.Path("./source")
-WWW_PATH = pathlib.Path("./www")
-WWW_MEDIA_PATH = WWW_PATH / "media"
+_SOURCE_PATH = pathlib.Path("./source")
+_WWW_PATH = pathlib.Path("./www")
+_WWW_MEDIA_PATH = _WWW_PATH / "media"
+_PLUGIN_PATH = pathlib.Path("./plugins")
+
+_PANDOC_FILE_EXTENSIONS = (".md", ".rst")
 
 
 app = fastapi.FastAPI()
@@ -29,52 +32,56 @@ app = fastapi.FastAPI()
 @app.get("/api/source")
 async def get_source_files():
     """get list of source file names"""
-    return [file.name for file in SOURCE_PATH.glob("*")]
+    return [file.name for file in _SOURCE_PATH.glob("*")]
 
 
 @app.get("/api/source/{name}")
 async def get_source(name: str):
     """get source source for a html page"""
-    source_file_path = SOURCE_PATH / name
+    source_file_path = _SOURCE_PATH / name
 
-    if not source_file_path.is_relative_to(SOURCE_PATH):
+    if not source_file_path.is_relative_to(_SOURCE_PATH):
         raise fastapi.HTTPException(status_code=403, detail="not allowed")
 
-    with open(source_file_path, "r", encoding="utf-8") as file:
-        return file.read()
+    return source_file_path.read_text()
 
 
 @app.delete("/api/source/{name}")
 async def delete_source_file(name: str):
     """delete source file and corresponding html page"""
-    # check if html file is valid
-    html_file_path = WWW_PATH.joinpath(name).with_suffix(".html")
-
-    if not html_file_path.is_relative_to(WWW_PATH):
-        raise fastapi.HTTPException(status_code=403, detail="not allowed")
 
     # check if source file is valid
-    source_file_path = SOURCE_PATH.joinpath(name)
+    source_file_path = _SOURCE_PATH / name
 
-    if not source_file_path.is_relative_to(SOURCE_PATH):
+    if not source_file_path.is_relative_to(_SOURCE_PATH):
         raise fastapi.HTTPException(status_code=403, detail="not allowed")
 
-    # delet files
-    html_file_path.unlink(missing_ok=True)
+    www_file_path = None
+
+    if source_file_path.suffix in _PANDOC_FILE_EXTENSIONS:
+        # check if html file is valid
+        www_file_path = _WWW_PATH.joinpath(name).with_suffix(".html")
+
+    else:
+        www_file_path = _WWW_PATH / name
+
+    if not www_file_path.is_relative_to(_WWW_PATH):
+        raise fastapi.HTTPException(status_code=403, detail="not allowed")
+
+    # delete files
+    www_file_path.unlink(missing_ok=True)
     source_file_path.unlink()
 
 
-#@app.put("/api/source/{name}", response_class=fastapi.responses.HTMLResponse)
 @app.put("/api/source/{name}")
-async def put_source_file(name: str, source: SourceData) -> None:
+async def put_source_file(name: str, source: SourceData):
     """put source file and create corresponding html page"""
+    name, data = plugin.pre_put_source_file(name, source.data)
+
     # check if source file path is valid
-    source_file_path = SOURCE_PATH / name
+    source_file_path = _SOURCE_PATH / name
 
-    if not source_file_path.is_relative_to(SOURCE_PATH):
-        raise fastapi.HTTPException(status_code=403, detail="not allowed")
-
-    if source_file_path.name == "manage.md": # manage.html is a special page
+    if not source_file_path.is_relative_to(_SOURCE_PATH):
         raise fastapi.HTTPException(status_code=403, detail="not allowed")
 
     # backup old source file with same name, if present
@@ -83,38 +90,31 @@ async def put_source_file(name: str, source: SourceData) -> None:
     except FileNotFoundError:
         pass
 
-    # sanitize input and write source file
-    with open(source_file_path, "w", encoding="utf-8") as file:
-        # todo: sanitizer removes whitespaces
-        #sanitized_html = html_sanitizer.Sanitizer({"keep_typographic_whitespace": True}).sanitize(source.data)
-        #sanitized_html = source.data
-        file.write(source.data)
+    # write source file
+    source_file_path.write_text(data, encoding="utf-8")
 
     # process new source file
-    _process_source_file(source_file_path)
+    www_file_path = _process_source_file(source_file_path)
 
-    #if source_file_path.suffix != ".css":
-    #    css_files = [css_file.name for css_file in SOURCE_PATH.glob("*.css")]
+    plugin.post_put_source_file(source_file_path, www_file_path)
 
-    #    if not _create_html_page(name, source_file_path, css_files):
-    #        raise fastapi.HTTPException(status_code=400, detail="error in source")
-
+    return source_file_path.read_text(encoding="utf-8")
 
 
 @app.get("/api/media")
 async def get_media_files():
     """get list of media files"""
-    return [file.name for file in WWW_MEDIA_PATH.glob("*")]
+    return [file.name for file in _WWW_MEDIA_PATH.glob("*")]
 
 
 @app.put("/api/media")
 async def upload_media_files(files: list[fastapi.UploadFile]):
     """upload multiple media files"""
     for remote_file in files:
-        local_file_path = WWW_MEDIA_PATH / remote_file.filename
+        local_file_path = _WWW_MEDIA_PATH / remote_file.filename
 
         # check if media file path is valid
-        if not local_file_path.is_relative_to(WWW_MEDIA_PATH):
+        if not local_file_path.is_relative_to(_WWW_MEDIA_PATH):
             raise fastapi.HTTPException(status_code=403, detail="not allowed")
 
         # backup old media file with same name, if present
@@ -123,41 +123,41 @@ async def upload_media_files(files: list[fastapi.UploadFile]):
         except FileNotFoundError:
             pass
 
-        with open(local_file_path, "wb") as local_file:
-            remote_content = await remote_file.read()
-            local_file.write(remote_content)
+        local_file_path.write_bytes(await remote_file.read())
 
 
 @app.delete("/api/media/{name}")
 async def delete_media_file(name: str):
     """delete media file"""
     # check if media file is valid
-    file_path = WWW_MEDIA_PATH.joinpath(name)
+    file_path = _WWW_MEDIA_PATH.joinpath(name)
 
-    if not file_path.is_relative_to(WWW_MEDIA_PATH):
+    if not file_path.is_relative_to(_WWW_MEDIA_PATH):
         raise fastapi.HTTPException(status_code=403, detail="not allowed")
 
     # delete media file
     file_path.unlink()
 
 
-def _process_source_file(source_file_path: pathlib.Path) -> bool:
+def _process_source_file(source_file_path: pathlib.Path) -> pathlib.Path:
     """create html files from markup sources, then copy files to www directory"""
-
     # check if we need go generate html by invoking pandoc
-    if source_file_path.suffix  in (".md", ".rst"):
-        # get all currently availabe css files
-        css_files = [css_file.name for css_file in SOURCE_PATH.glob("*.css")]
+    if source_file_path.suffix in _PANDOC_FILE_EXTENSIONS:
+        # create html page with currently available css files
+        css_files = [css_file.name for css_file in _SOURCE_PATH.glob("*.css")]
+        html = _create_html(source_file_path, css_files)
 
-        return _create_html_page(source_file_path, css_files)
+        www_file_path = _WWW_PATH.joinpath(source_file_path.stem).with_suffix(".html")
+        www_file_path.write_text(html, encoding="utf-8")
+    else:
+        # other files are copied to www directory
+        www_file_path = _WWW_PATH / source_file_path.name
+        www_file_path.write_bytes(source_file_path.read_bytes())
 
-    # copy other files to www directory
-    www_file_path = WWW_PATH / source_file_path.name
-    www_file_path.write_bytes(source_file_path.read_bytes())
-    return True
+    return www_file_path
 
 
-def _create_html_page(source_file_path: pathlib.Path, css_files: list[str]) -> bool:
+def _create_html(source_file_path: pathlib.Path, css_files: list[str]) -> str:
     run_params = ["pandoc", "--standalone", "--to", "html5"]
 
     for css_file in css_files:
@@ -170,23 +170,21 @@ def _create_html_page(source_file_path: pathlib.Path, css_files: list[str]) -> b
     if result.returncode != 0:
         return False
 
-    html_file_data = result.stdout.decode("utf-8")
+    html_data = result.stdout.decode("utf-8")
 
-    html_path = WWW_PATH.joinpath(source_file_path.stem).with_suffix(".html")
-
-    with open(html_path, "w", encoding="utf-8") as file:
-        file.write(html_file_data)
-
-    return True
+    return html_data
 
 
 # create directories if not present
-SOURCE_PATH.mkdir(exist_ok=True)
-WWW_PATH.mkdir(exist_ok=True)
-WWW_MEDIA_PATH.mkdir(exist_ok=True)
+_SOURCE_PATH.mkdir(exist_ok=True)
+_WWW_PATH.mkdir(exist_ok=True)
+_WWW_MEDIA_PATH.mkdir(exist_ok=True)
+
+# load plugins
+plugin.load(_PLUGIN_PATH, _SOURCE_PATH, _WWW_PATH, _WWW_MEDIA_PATH)
 
 # make fastapi serve static (html) files
-app.mount("/", fastapi.staticfiles.StaticFiles(directory=WWW_PATH, html=True), name="www")
+app.mount("/", fastapi.staticfiles.StaticFiles(directory=_WWW_PATH, html=True), name="www")
 
 if __name__ == "__main__":
     # parse command line arguments

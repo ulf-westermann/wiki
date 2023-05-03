1281fba7d0d453b22bdb37f39f94ed9b17ec143e
FreeBSD User <freebsd@fog.fritz.box>
2023-05-03 12:22:28 +0000

    update

--- run.sh
+++ run.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 
-# trap is executed when shell receives any signal that stops the shell.
-# trap kills any child python processes, but they need to run in background
-# to execute the trap.
+# trap is executed when shell receives any signal to stop the shell.
+# trap kills any python child processes, but they need to run in
+# background to execute the trap (otherwise the sh process is blocked).
 trap "pkill -P $$ -f python" TERM KILL QUIT STOP INT
 
-cd /srv/ulf/wiki # enter wiki directory
-. ./venv/bin/activate # activate python virtual environment
-python3.9 -m wiki & # run python wiki in background
-pid=$! # get pid of last started job (python)
-wait $pid # wait until python stops
+# enter wiki directory
+cd /srv/ulf/wiki
+
+# activate python virtual environment
+. ./venv/bin/activate
+
+# run python wiki in background
+python3.9 -m wiki &
+
+# get pid of last started job (python)
+pid=$!
+
+# wait until python stops
+wait $pid

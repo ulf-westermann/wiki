7aba5008a89ee8137952fe9abad85fa037643d88
ulf <mail@ulf-westermann.de>
2024-02-27 21:47:07 +0100

    optimized http url parsing

--- plugin_repository/links.py
+++ plugin_repository/links.py
@@ -20,27 +20,28 @@ class Plugin(plugin.PluginBase):
             url_string = line.strip()
 
             # check if line contains a url
-            result = urllib.parse.urlparse(url_string)
+            if line.startswith("http"):
+                result = urllib.parse.urlparse(url_string)
 
-            if all((result.scheme, result.netloc)):
-                timetag = datetime.datetime.now().isoformat('T', 'seconds')
+                if all((result.scheme, result.netloc)):
+                    timetag = datetime.datetime.now().isoformat('T', 'seconds')
 
-                try:
-                    # open url
-                    with urllib.request.urlopen(urllib.request.Request(url_string, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"})) as url:
-                        # parse html
-                        soup = bs4.BeautifulSoup(url, features="html.parser")
+                    try:
+                        # open url
+                        with urllib.request.urlopen(urllib.request.Request(url_string, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"})) as url:
+                            # parse html
+                            soup = bs4.BeautifulSoup(url, features="html.parser")
 
-                        title = soup.title.string.replace("\r", " ").replace("\n", " ").strip()
+                            title = soup.title.string.replace("\r", " ").replace("\n", " ").strip()
 
-                        description_tag = soup.find("meta", property="og:description")
-                        description = ", " + description_tag["content"].replace("\r", " ").replace("\n", " ").strip() if description_tag else ""
+                            description_tag = soup.find("meta", property="og:description")
+                            description = ", " + description_tag["content"].replace("\r", " ").replace("\n", " ").strip() if description_tag else ""
 
-                        # substitute url by markdown link
-                        lines[count] = f"[{title} <small>({timetag}, {result.netloc}{description})</small>]({url_string})"
-                except Exception as exc:
-                    print(exc)
-                    lines[count] = f"[{url_string} <small>({timetag}, {result.netloc})</small>]({url_string})"
+                            # substitute url by markdown link
+                            lines[count] = f"[{title} <small>({timetag}, {result.netloc}{description})</small>]({url_string})"
+                    except Exception as exc:
+                        print(exc)
+                        lines[count] = f"[{url_string} <small>({timetag}, {result.netloc})</small>]({url_string})"
 
         lines = "\n".join(lines)
 
